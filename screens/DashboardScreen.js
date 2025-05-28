b4edbf26fd3552c17af7592889861b106ce86efa
lauraedito <113810506+lauraedito@users.noreply.github.com>
2025-05-28 09:06:45 -0700

    cita dash

--- screens/DashboardScreen.js
+++ screens/DashboardScreen.js
@@ -22,6 +22,24 @@ const DashboardScreen = () => {
         />
         <Text style={styles.title}>Perruqueria</Text>
       </View>
+
+      <Text style={styles.date}>Próxima cita</Text>
+            <View style={styles.card}>
+        <View style={styles.cardHeader}>
+          <Text style={styles.cardTitle}>Corte de cabello</Text>
+          <View style={styles.icons}>
+            <TouchableOpacity>
+              <Text style={styles.pIcon}>🐾</Text>
+            </TouchableOpacity>
+            <TouchableOpacity>
+             
+            </TouchableOpacity>
+          </View>
+        </View>
+        <Text style={styles.status}>Confirmada</Text>
+        <Text style={styles.time}>Mañana, 10:00 AM</Text>
+</View>
+
     </ScrollView>
   );
 };
@@ -46,7 +64,58 @@ const styles = StyleSheet.create({
     width: 40,
     height: 40,
     resizeMode: 'contain',
-  }
+  },
+  card: {
+  backgroundColor: "#fff",
+  borderRadius: 10,
+  padding: 15,
+  marginTop: 20,
+  shadowColor: "#000",
+  shadowOffset: { width: 0, height: 2 },
+  shadowOpacity: 0.1,
+  shadowRadius: 5,
+  elevation: 3,
+},
+
+cardHeader: {
+  flexDirection: "row",
+  justifyContent: "space-between",
+  alignItems: "center",
+},
+
+cardTitle: {
+  fontWeight: "bold",
+  fontSize: 16,
+  color: "#000",
+},
+
+// icons: {
+//   flexDirection: "row",
+//   gap: 10,
+// },
+
+// editIcon: {
+//   fontSize: 16,
+//   color: "#555",
+//   marginRight: 10,
+// },
+
+// deleteIcon: {
+//   fontSize: 16,
+//   color: "red",
+// },
+
+status: {
+  marginTop: 8,
+  fontSize: 14,
+  color: "#333",
+},
+
+time: {
+  fontSize: 14,
+  color: "#555",
+},
+
 });
 
 export default DashboardScreen;
