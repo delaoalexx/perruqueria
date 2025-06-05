aa3df3703f0c9f92750a1f882278718299451126
Alexandra <adelao_22@alu.uabcs.mx>
2025-06-04 22:49:44 -0700

    ADD getProductsByCategory

--- services/productsService.js
+++ services/productsService.js
@@ -8,6 +8,12 @@ export const addProduct = async (data) => {
   return docRef.id;
 };
 
+export const getProductsByCategory = async (categoryId) => {
+  const q = query(productsCollection, where('categoryId', '==', categoryId));
+  const snapshot = await getDocs(q);
+  return snapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
+};
+
 export const getProducts = async () => {
   const snapshot = await getDocs(productsCollection);
   return snapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
@@ -17,6 +23,8 @@ export const updateProduct = async (id, data) => {
   const productDoc = doc(db, 'products', id);
   await updateDoc(productDoc, data);
 };
+
+
 
 export const deleteProduct = async (id) => {
   const productDoc = doc(db, 'products', id);
